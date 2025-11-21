ab976b57b3334fb63621ef8e986808842cffb54c
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2025-11-21 11:21:40 +0100

    fix: update image optimization

--- components/ComponentProviders.tsx
+++ components/ComponentProviders.tsx
@@ -50,6 +50,7 @@ import { useImageSize } from "./ImageSizeContext";
 import { NavTopbarProvider } from "./nav-topbar/NavTopbarComponent";
 import { NavFlyoutProvider } from "./nav-flyout/NavFlyoutComponent";
 import { FooterProvider } from "./footer/FooterComponent";
+import { useImageRatio } from "./ImageRatioContext";
 
 const Link = forwardRef<
   HTMLAnchorElement,
@@ -77,6 +78,7 @@ const Picture = forwardRef<
   const blurHashes = useBlurHashes();
   const priority = useImagePriority();
   const size = useImageSize();
+  const ratio = useImageRatio();
 
   useImperativeHandle<HTMLImageElement | null, HTMLImageElement | null>(
     ref,
@@ -87,9 +89,8 @@ const Picture = forwardRef<
     if (internalRef.current) resetBackgroundBlurHash(internalRef.current);
   }, []);
 
-  if (!src || (isStoryblokAsset(src) && !src.filename)) return;
-  const source = isStoryblokAsset(src) ? src.filename : src;
-  const fileUrl = !source.startsWith("http") ? `https:${source}` : source;
+  if (!src) return;
+  const fileUrl = !src.startsWith("http") ? `https:${src}` : src;
   const [width, height] = fileUrl.match(/\/(\d+)x(\d+)\//)?.slice(1) || [];
   const maxWidth = parseInt(width) > size ? Math.floor(size) : parseInt(width);
   const maxHeight =
@@ -105,7 +106,7 @@ const Picture = forwardRef<
       src={fileUrl}
       width={maxWidth}
       height={maxHeight}
-      alt={isStoryblokAsset(src) && src.alt ? src.alt : props.alt || ""}
+      alt={props.alt || ""}
       lazy={priority ? false : lazy}
       fetchPriority="high"
       loading={priority ? "eager" : "lazy"}
@@ -114,7 +115,7 @@ const Picture = forwardRef<
     <Image
       ref={internalRef}
       {...props}
-      alt={isStoryblokAsset(src) && src.alt ? src.alt : props.alt || ""}
+      alt={props.alt || ""}
       src={
         priority
           ? `${fileUrl}/${
@@ -122,8 +123,10 @@ const Picture = forwardRef<
             }filters:quality(50)`
           : fileUrl
       }
-      width={autoSize ? undefined : maxWidth}
-      height={autoSize ? undefined : maxHeight}
+      layout={autoSize ? "fullWidth" : "constrained"}
+      aspectRatio={ratio > 0 ? ratio : undefined}
+      width={maxWidth}
+      height={autoSize || ratio > 0 ? undefined : maxHeight}
       priority={lazy === false || priority}
       onLoad={(event) => {
         if (event.target instanceof HTMLImageElement) {
@@ -153,35 +156,28 @@ const Hero = forwardRef<
 
   const src =
     (image &&
-      ((image.src &&
-        isStoryblokAsset(image.src) &&
-        `${image.src.filename}/m/600x0`) ||
-        image.src)) ||
+      (image.src && !image.src.endsWith("/m/600x0")
+        ? `${image.src}/m/600x0`
+        : image.src)) ||
     undefined;
   const srcMobile =
     (image &&
-      ((image.srcMobile &&
-        isStoryblokAsset(image.srcMobile) &&
-        image.srcMobile.filename &&
-        `${image.srcMobile.filename}/m/600x0`) ||
-        image.srcMobile)) ||
+      (image.srcMobile && !image.srcMobile.endsWith("/m/600x0")
+        ? `${image.srcMobile}/m/600x0`
+        : image.srcMobile)) ||
     src ||
     "";
   const srcTablet =
     (image &&
-      ((image.srcTablet &&
-        isStoryblokAsset(image.srcTablet) &&
-        image.srcTablet.filename &&
-        `${image.srcTablet.filename}/m/950x0`) ||
-        image.srcTablet)) ||
+      (image.srcTablet && !image.srcTablet.endsWith("/m/950x0")
+        ? `${image.srcTablet}/m/950x0`
+        : image.srcTablet)) ||
     undefined;
   const srcDesktop =
     (image &&
-      ((image.srcDesktop &&
-        isStoryblokAsset(image.srcDesktop) &&
-        image.srcDesktop.filename &&
-        `${image.srcDesktop.filename}/m/1600x0`) ||
-        image.srcDesktop)) ||
+      (image.srcDesktop && !image.srcDesktop.endsWith("/m/1600x0")
+        ? `${image.srcDesktop}/m/1600x0`
+        : image.srcDesktop)) ||
     undefined;
 
   return (
@@ -210,11 +206,7 @@ const Storytelling = forwardRef<
   return (
     <StorytellingContextDefault
       {...props}
-      backgroundImage={
-        isStoryblokAsset(backgroundImage)
-          ? backgroundImage.filename
-          : backgroundImage
-      }
+      backgroundImage={backgroundImage}
       ref={ref}
     />
   );
--- components/ImageSizeProviders.tsx
+++ components/ImageSizeProviders.tsx
@@ -1,9 +1,12 @@
 import {
+  Children,
   ComponentProps,
   FC,
   forwardRef,
   HTMLAttributes,
   PropsWithChildren,
+  useContext,
+  useMemo,
 } from "react";
 import { ImageSizeProvider, useImageSize } from "./ImageSizeContext";
 import {
@@ -20,82 +23,104 @@ import {
 } from "@kickstartds/ds-agency-premium/components/image-story/index.js";
 import calculated from "@/token/calculated";
 
-const Section = forwardRef<
-  HTMLDivElement,
-  ComponentProps<typeof SectionContextDefault> &
-    Omit<HTMLAttributes<HTMLElement>, "style" | "content">
->((props, ref) => {
-  const sectionWidthName =
-    props.content?.width === "unset"
-      ? props.width || "default"
-      : calculated.sectionWidths[props.content?.width || "default"] >
-        calculated.sectionWidths[props.width || "default"]
-      ? props.width || "default"
-      : props.content?.width || "default";
-  const sectionWidth =
-    calculated.sectionWidths[sectionWidthName] * calculated.baseFontSizePx;
+const SectionProvider: FC<PropsWithChildren> = (props) => {
+  const UpstreamSection = useContext(SectionContext);
 
-  const componentWidth =
-    props.content?.mode === "list"
-      ? sectionWidth
-      : props.content?.mode === "slider"
-      ? sectionWidth
-      : sectionWidth / 2;
+  const Section = useMemo(
+    () =>
+      forwardRef<
+        HTMLDivElement,
+        ComponentProps<typeof SectionContextDefault> &
+          Omit<HTMLAttributes<HTMLElement>, "style" | "content">
+      >(function SectionImageSize(props, ref) {
+        // TODO should also take into account section gap width
+        const childCount = Children.count(props.children) || 1;
 
-  return (
-    <ImageSizeProvider size={componentWidth}>
-      <SectionContextDefault {...props} ref={ref} />
-    </ImageSizeProvider>
-  );
-});
-Section.displayName = "Section";
+        const sectionWidthName =
+          props.content?.width === "unset" || !props.content?.width
+            ? props.width || "default"
+            : calculated.sectionWidths[props.content?.width || "default"] >
+              calculated.sectionWidths[props.width || "default"]
+            ? props.width || "default"
+            : props.content?.width || "default";
 
-const SectionProvider: FC<PropsWithChildren> = (props) => (
-  <SectionContext.Provider {...props} value={Section} />
-);
+        const sectionWidth =
+          calculated.sectionWidths[sectionWidthName] *
+          calculated.baseFontSizePx;
 
-const Logos = forwardRef<
-  HTMLDivElement,
-  ComponentProps<typeof LogosContextDefault> & HTMLAttributes<HTMLDivElement>
->((props, ref) => {
-  const size = useImageSize();
-  const gapSize = calculated.desktop["--dsa-logos__grid--gap-horizontal"];
-  const logoSize = Math.ceil(
-    (size - gapSize * (props.logosPerRow || 3)) / (props.logosPerRow || 3)
+        const componentWidth =
+          props.content?.mode === "list"
+            ? sectionWidth
+            : props.content?.mode === "slider"
+            ? sectionWidth
+            : sectionWidth / childCount;
+
+        return (
+          <ImageSizeProvider size={componentWidth}>
+            <UpstreamSection {...props} ref={ref} />
+          </ImageSizeProvider>
+        );
+      }),
+    [UpstreamSection]
   );
 
-  return (
-    <ImageSizeProvider size={logoSize}>
-      <LogosContextDefault {...props} ref={ref} />
-    </ImageSizeProvider>
+  return <SectionContext.Provider {...props} value={Section} />;
+};
+
+const LogosProvider: FC<PropsWithChildren> = (props) => {
+  const UpstreamLogos = useContext(LogosContext);
+
+  const Logos = useMemo(
+    () =>
+      forwardRef<
+        HTMLDivElement,
+        ComponentProps<typeof LogosContextDefault> &
+          HTMLAttributes<HTMLDivElement>
+      >(function LogosImageSize(props, ref) {
+        const size = useImageSize();
+        const gapSize = calculated.desktop["--dsa-logos__grid--gap-horizontal"];
+        const logoSize = Math.ceil(
+          (size - gapSize * (props.logosPerRow || 3)) / (props.logosPerRow || 3)
+        );
+
+        return (
+          <ImageSizeProvider size={logoSize}>
+            <UpstreamLogos {...props} ref={ref} />
+          </ImageSizeProvider>
+        );
+      }),
+    [UpstreamLogos]
   );
-});
-Logos.displayName = "Logos";
 
-const LogosProvider: FC<PropsWithChildren> = (props) => (
-  <LogosContext.Provider {...props} value={Logos} />
-);
+  return <LogosContext.Provider {...props} value={Logos} />;
+};
 
-const ImageStory = forwardRef<
-  HTMLDivElement,
-  ComponentProps<typeof ImageStoryContextDefault> &
-    HTMLAttributes<HTMLDivElement>
->((props, ref) => {
-  const size = useImageSize();
-  const gapSize = calculated.phone["--dsa-image-story--horizontal-padding"];
-  const imageSize = Math.ceil(size / 2 - gapSize);
+const ImageStoryProvider: FC<PropsWithChildren> = (props) => {
+  const UpstreamImageStory = useContext(ImageStoryContext);
 
-  return (
-    <ImageSizeProvider size={imageSize}>
-      <ImageStoryContextDefault {...props} ref={ref} />
-    </ImageSizeProvider>
+  const ImageStory = useMemo(
+    () =>
+      forwardRef<
+        HTMLDivElement,
+        ComponentProps<typeof ImageStoryContextDefault> &
+          HTMLAttributes<HTMLDivElement>
+      >(function ImageStoryImageSize(props, ref) {
+        const size = useImageSize();
+        const gapSize =
+          calculated.phone["--dsa-image-story--horizontal-padding"];
+        const imageSize = Math.ceil(size / 2 - gapSize);
+
+        return (
+          <ImageSizeProvider size={imageSize}>
+            <UpstreamImageStory {...props} ref={ref} />
+          </ImageSizeProvider>
+        );
+      }),
+    [UpstreamImageStory]
   );
-});
-ImageStory.displayName = "ImageStory";
 
-const ImageStoryProvider: FC<PropsWithChildren> = (props) => (
-  <ImageStoryContext.Provider {...props} value={ImageStory} />
-);
+  return <ImageStoryContext.Provider {...props} value={ImageStory} />;
+};
 
 const ImageSizeProviders = (props: PropsWithChildren) => (
   <SectionProvider>
